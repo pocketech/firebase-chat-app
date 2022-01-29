b369e3425e96061a678fb917b97631e6d8e80939
tw-nakano <nakano@tailorworks.com>
2022-01-29 22:07:21 +0900

    :wrench:

---
+++ src/libs/firebase.ts
@@ -0,0 +1,24 @@
+import type { FirebaseOptions } from 'firebase/app'
+import { getApp, getApps } from 'firebase/app'
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+import { getStorage } from 'firebase/storage'
+
+const firebaseConfig: FirebaseOptions = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+}
+
+// NOTE: 初期化が一度だけ行われるように
+const firebaseApp = !getApps().length ? initializeApp(firebaseConfig) : getApp()
+
+// eslint-disable-next-line import/no-default-export
+export default firebaseApp
+export { getAuth, getFirestore, getStorage }
