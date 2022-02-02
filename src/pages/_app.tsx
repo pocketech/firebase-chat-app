a3edfabf02d0c0601f2692cc6b248e422ef2186d
tw-nakano <nakano@tailorworks.com>
2022-02-03 08:47:57 +0900

    :wrench:

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,19 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
 
+import { AuthContextProvider } from '@/auth/context/AuthContextProvider'
+import { theme } from '@/theme'
+
 if (process.env.NEXT_PUBLIC_API_MOCKING === 'enabled') {
   require('../mocks')
 }
 const MyApp = ({ Component, pageProps }: AppProps) => {
+  const getLayout: (page: JSX.Element) => JSX.Element =
+    (Component as any).getLayout || ((page) => page)
+
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
+    <ChakraProvider theme={theme}>
+      <AuthContextProvider>{getLayout(<Component {...pageProps} />)}</AuthContextProvider>
     </ChakraProvider>
   )
 }
