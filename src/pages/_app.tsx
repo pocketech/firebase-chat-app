154f6033bacc129c0f8f01895cc1b4c68d7450f8
pocketech <hakuyowasedatech@gmail.com>
2022-02-03 09:11:56 +0900

    :heavy_minus_sign:mswを除去

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,9 +4,6 @@ import type { AppProps } from 'next/app'
 import { AuthContextProvider } from '@/auth/context/AuthContextProvider'
 import { theme } from '@/theme'
 
-if (process.env.NEXT_PUBLIC_API_MOCKING === 'enabled') {
-  require('../mocks')
-}
 const MyApp = ({ Component, pageProps }: AppProps) => {
   const getLayout: (page: JSX.Element) => JSX.Element =
     (Component as any).getLayout || ((page) => page)
