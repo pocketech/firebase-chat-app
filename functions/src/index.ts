3968ac473ee9388cd12a0c0f44d852b2843724ed
pocketech <hakuyowasedatech@gmail.com>
2022-02-07 10:14:31 +0900

    :sparkles:ユーザ作成時にfirestoreにデータ登録する関数を作成

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,30 @@
 import * as functions from "firebase-functions"
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", { structuredData: true })
-  response.send("Hello from Firebase!")
-})
+import * as admin from "firebase-admin"
+
+admin.initializeApp();
+
+exports.createUserDocument = functions.region('asia-northeast1').auth.user().onCreate(async user => {
+  const userUid = user.uid;
+  const userEmail = user.email;
+  
+  console.info(user.displayName)
+
+  // admin.auth().getUser() 経由で取得, 取得するまでawait
+  const authedUser = await admin.auth().getUser(userUid);
+  const userName = authedUser.displayName;
+  
+  const newUser = {
+    email: userEmail,
+    name: userName
+  };
+
+  return admin
+    .firestore()
+    .collection("users")
+    .doc(userUid)
+    .set(newUser);
+});
+
+
+
