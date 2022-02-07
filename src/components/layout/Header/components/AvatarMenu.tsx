9635b58801ba0b2395e28b3307cf0b1e8114e5d2
pocketech <hakuyowasedatech@gmail.com>
2022-02-08 08:57:48 +0900

    :lipstick:avatarmenuへのデータの受け渡し

--- src/components/layout/Header/components/AvatarMenu.tsx
+++ src/components/layout/Header/components/AvatarMenu.tsx
@@ -1,17 +1,15 @@
-import { Menu, MenuButton, MenuDivider, MenuItem, MenuList } from '@chakra-ui/react'
+import { Menu, MenuButton, MenuDivider, MenuGroup, MenuItem, MenuList } from '@chakra-ui/react'
 import { useRouter } from 'next/router'
 import { HiOutlineCog, HiOutlineLogout, HiOutlineUserCircle } from 'react-icons/hi'
 
 import { signOut } from '@/auth/api/signOut'
 import { Avatar } from '@/components/common/Avatar'
 import { pagesPath } from '@/libs/$path'
+import type { User } from '@/types/user'
 
-type Props = {
-  displayName: string
-  photoURL?: string
-}
+type Props = Pick<User, 'name' | 'avatarUrl'>
 
-export const AvatarMenu: React.VFC<Props> = ({ displayName, photoURL }) => {
+export const AvatarMenu: React.VFC<Props> = ({ name, avatarUrl }) => {
   const { push } = useRouter()
   const onClick = () => {
     signOut().then(() => {
@@ -27,19 +25,21 @@ export const AvatarMenu: React.VFC<Props> = ({ displayName, photoURL }) => {
       isLazy
     >
       <MenuButton aria-label="アカウントメニュー">
-        <Avatar size="sm" name={displayName} src={photoURL} />
+        <Avatar size="sm" name={name} src={avatarUrl} />
       </MenuButton>
 
       <MenuList>
-        <MenuItem
-          icon={<HiOutlineUserCircle fontSize="1.6em" color="#999" />}
-          onClick={() => push(pagesPath.profile.$url())}
-        >
-          プロフィール
-        </MenuItem>
-        <MenuItem icon={<HiOutlineCog fontSize="1.6em" color="#999" />} onClick={() => push('/')}>
-          アカウント設定
-        </MenuItem>
+        <MenuGroup title={name}>
+          <MenuItem
+            icon={<HiOutlineUserCircle fontSize="1.6em" color="#999" />}
+            onClick={() => push(pagesPath.profile.$url())}
+          >
+            プロフィール
+          </MenuItem>
+          <MenuItem icon={<HiOutlineCog fontSize="1.6em" color="#999" />} onClick={() => push('/')}>
+            アカウント設定
+          </MenuItem>
+        </MenuGroup>
         <MenuDivider />
         <MenuItem icon={<HiOutlineLogout fontSize="1.6em" color="#999" />} onClick={onClick}>
           ログアウト
--- src/components/layout/Header/components/Header.tsx
+++ src/components/layout/Header/components/Header.tsx
@@ -1,12 +1,16 @@
 import type { PositionProps } from '@chakra-ui/react'
 import { Button } from '@chakra-ui/react'
 import { Flex, Icon } from '@chakra-ui/react'
+import { doc } from 'firebase/firestore'
 import NextLink from 'next/link'
+import { useDocument } from 'react-firebase-hooks/firestore'
 import { HiOutlineBell } from 'react-icons/hi'
 
 import { useAuthUser } from '@/auth/hooks'
 import { Logo } from '@/components/common/Logo'
 import { pagesPath } from '@/libs/$path'
+import { db } from '@/libs/firebase'
+import type { User } from '@/types/user'
 
 import { GUTTER } from '../../constants'
 import { AvatarMenu } from './AvatarMenu'
@@ -15,6 +19,10 @@ type Props = PositionProps
 
 export const Header: React.VFC<Props> = ({ ...positionProps }) => {
   const { authenticatedUser } = useAuthUser()
+  const [snapshot] = useDocument(
+    authenticatedUser ? doc(db, 'users', authenticatedUser.uid) : undefined
+  )
+  const user = snapshot?.data() as User | undefined
 
   return (
     <Flex
@@ -26,13 +34,10 @@ export const Header: React.VFC<Props> = ({ ...positionProps }) => {
       {...positionProps}
     >
       <Logo width={{ base: '40', lg: '32' }} />
-      {authenticatedUser ? (
+      {user ? (
         <Flex ml="auto" align="center" gridGap="4">
           <Icon as={HiOutlineBell} boxSize="6" color="gray.400" />
-          <AvatarMenu
-            displayName={authenticatedUser.displayName!}
-            photoURL={authenticatedUser.photoURL ?? undefined}
-          />
+          <AvatarMenu name={user.name} avatarUrl={user.avatarUrl} />
         </Flex>
       ) : (
         <NextLink href={pagesPath.signup.$url()} passHref>
