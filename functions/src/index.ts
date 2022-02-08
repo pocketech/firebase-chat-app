6e1f1a51067d4266c645a9e8c5076a0b738acfde
pocketech <hakuyowasedatech@gmail.com>
2022-02-08 09:00:55 +0900

    :hammer:firestoreの変更がauthに波及するように

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,11 +2,11 @@ import * as functions from "firebase-functions"
 
 import * as admin from "firebase-admin"
 
+
 admin.initializeApp();
 
 exports.createUserDocument = functions.region('asia-northeast1').auth.user().onCreate(async user => {
   const userUid = user.uid;
-  const userEmail = user.email;
   
   console.info(user.displayName)
 
@@ -14,9 +14,8 @@ exports.createUserDocument = functions.region('asia-northeast1').auth.user().onC
   const authedUser = await admin.auth().getUser(userUid);
   const userName = authedUser.displayName;
   
-  const newUser = {
-    email: userEmail,
-    name: userName
+  const newUser= {
+    name: userName,
   };
 
   return admin
@@ -24,6 +23,16 @@ exports.createUserDocument = functions.region('asia-northeast1').auth.user().onC
     .collection("users")
     .doc(userUid)
     .set(newUser);
+});
+
+exports.updateDisplayNameAndPhotoURL = functions.region('asia-northeast1').firestore.document('users/{userId}').onUpdate(async (change, context) => {
+  const newName = change.after.data().name
+  const newAvatarUrl = change.after.data().avatarUrl
+  
+  console.info(newName)
+
+  return admin.auth().updateUser(context.params.userId, {displayName: newName, photoURL: newAvatarUrl})
+
 });
 
 
