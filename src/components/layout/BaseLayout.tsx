6112c5012d4b1a791f1f07fa2225d24a71f12558
pocketech <hakuyowasedatech@gmail.com>
2022-02-10 15:45:56 +0900

    :lipstick:レイアウトコンポーネントの改修

--- src/components/layout/BaseLayout.tsx
+++ src/components/layout/BaseLayout.tsx
@@ -5,11 +5,24 @@ import { Global } from '@emotion/react'
 import { GUTTER } from './constants'
 import { Header } from './Header'
 
-type Props = {
-  contentWidth?: LayoutProps['maxW']
-}
+type Props =
+  | {
+      hasContainer?: false
+      containerWidth?: undefined
+    } //コンテナがない場合は幅を指定できない
+  | {
+      hasContainer: true
+      containerWidth?: LayoutProps['maxW']
+    } //コンテナがある場合にのみ幅を指定できる
 
-export const BaseLayout: React.FC<Props> = ({ children, contentWidth = 'container.md' }) => {
+/**
+ * ログイン後に使用するベースとなるレイアウトコンポーネント。
+ */
+export const BaseLayout: React.FC<Props> = ({
+  children,
+  containerWidth = 'container.md',
+  hasContainer = false,
+}) => {
   return (
     <>
       <Global
@@ -22,16 +35,20 @@ export const BaseLayout: React.FC<Props> = ({ children, contentWidth = 'containe
 
       <Header position={{ lg: 'sticky' }} top="0" zIndex="sticky" />
 
-      <Container
-        as="main"
-        maxW={contentWidth}
-        px={{ ...GUTTER, '2xl': 0 }}
-        mt="8"
-        flex="1"
-        mx="auto"
-      >
-        {children}
-      </Container>
+      {hasContainer ? (
+        <Container
+          as="main"
+          maxW={containerWidth}
+          px={{ ...GUTTER, '2xl': 0 }}
+          mt="8"
+          flex="1"
+          mx="auto"
+        >
+          {children}
+        </Container>
+      ) : (
+        children
+      )}
     </>
   )
 }
