0f0cc8ccbab91b5921c8221752ec6d501cfe5e88
pocketech <hakuyowasedatech@gmail.com>
2022-02-11 21:40:47 +0900

    :sparkles:最新のメッセージを更新するfunctionを追加

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -35,5 +35,18 @@ exports.updateDisplayNameAndPhotoURL = functions.region('asia-northeast1').fires
 
 });
 
+exports.updateRecentMessage = functions.region('asia-northeast1').firestore.document('chats/{chatId}/messages/{messageId}').onCreate((snap, context) => {
+  const newMessage = snap.data()
+
+  const docRef = admin.firestore().collection("chats").doc(context.params.chatId)
+
+  return docRef.update({
+    recentMessage : {
+      body: newMessage.body || (newMessage.attachmentFileUrls && '画像が送信されました'),
+      createdAt: newMessage.createdAt
+    }
+  })
+})
+
 
 
