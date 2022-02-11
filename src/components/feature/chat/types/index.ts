ec16984d6b728c142a7168b916ddb4474751975c
pocketech <hakuyowasedatech@gmail.com>
2022-02-11 11:32:15 +0900

    :label:

---
+++ src/components/feature/chat/types/index.ts
@@ -0,0 +1,22 @@
+import type { User } from '@/types/user'
+
+export type Message = {
+  id: string
+  createdAt: string
+  updatedAt: string
+  body: string
+  author: User
+}
+
+export type Chat = {
+  id: string
+  createdAt: Date
+  updatedAt: Date
+  name?: string
+  recentMessage?: {
+    body: string
+  }
+  // 自分も含めたユーザIDの配列
+  memberIds: string[]
+  createdBy: string
+}
