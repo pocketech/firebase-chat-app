ff131c45f59965cdf3fc1c413f9f8a7cbd881ffe
pocketech <hakuyowasedatech@gmail.com>
2022-02-13 00:44:32 +0900

    :bug:ogp画像がうまく表示されないのを修正

--- src/utils/getAbsoluteURL.ts
+++ src/utils/getAbsoluteURL.ts
@@ -1,7 +1,7 @@
 // Gets the URL for the current environment
 export const getAbsoluteURL = (relativePath: `/${string}`) => {
   const baseURL = process.env.NEXT_PUBLIC_VERCEL_URL
-    ? process.env.NEXT_PUBLIC_VERCEL_URL
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
     : 'http://localhost:3039'
 
   return baseURL + relativePath
