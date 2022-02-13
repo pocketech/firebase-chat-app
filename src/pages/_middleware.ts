e658974618b7484186402e6f08c44f6979ef60c4
pocketech <hakuyowasedatech@gmail.com>
2022-02-14 00:04:14 +0900

    :closed_lock_with_key:Basic認証の導入

---
+++ src/pages/_middleware.ts
@@ -0,0 +1,23 @@
+import type { NextRequest } from 'next/server'
+import { NextResponse } from 'next/server'
+
+export const middleware = (req: NextRequest) => {
+  const basicAuth = req.headers.get('authorization')
+
+  if (basicAuth) {
+    const auth = basicAuth.split(' ')[1]
+    const [user, password] = Buffer.from(auth, 'base64').toString().split(':')
+
+    if (user === process.env.BASIC_AUTH_USER && password === process.env.BASIC_AUTH_PASSWORD) {
+      return NextResponse.next()
+    }
+  }
+
+  return new Response('Auth required', {
+    status: 401,
+    headers: {
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      'WWW-Authenticate': 'Basic realm="Secure Area"',
+    },
+  })
+}
