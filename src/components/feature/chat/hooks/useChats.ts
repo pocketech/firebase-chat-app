e945da73fbe7e946ac9d2b242a321f070cf19238
pocketech <hakuyowasedatech@gmail.com>
2022-02-16 08:41:55 +0900

    :bug:チャットリストが最新順で表示されるように

--- src/components/feature/chat/hooks/useChats.ts
+++ src/components/feature/chat/hooks/useChats.ts
@@ -8,21 +8,31 @@ import type { Chat } from '../types'
 export const useChats = (userId: string | undefined) => {
   const [snapshot, isLoading, error] = useCollection(
     userId
-      ? query(collection(db, 'chats'), where('memberIds', 'array-contains', userId))
+      ? query(
+          collection(db, 'chats'),
+          where('memberIds', 'array-contains', userId)
+          // orderBy('recentMessage.createdAt', 'desc')
+        )
       : undefined
   )
 
   const chats = snapshot
-    ? (snapshot.docs.map((doc) => ({
-        ...doc.data(),
-        id: doc.id,
-        createdAt: doc.data({ serverTimestamps: 'estimate' }).createdAt.toDate(),
-        updatedAt: doc.data({ serverTimestamps: 'estimate' }).updatedAt.toDate(),
-        recentMessage: {
-          body: doc.data().recentMessage?.body,
-          createdAt: doc.data().recentMessage?.createdAt.toDate(),
-        },
-      })) as Chat[])
+    ? (
+        snapshot.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+          createdAt: doc.data({ serverTimestamps: 'estimate' }).createdAt.toDate(),
+          updatedAt: doc.data({ serverTimestamps: 'estimate' }).updatedAt.toDate(),
+          recentMessage: {
+            body: doc.data().recentMessage?.body,
+            createdAt: doc.data().recentMessage?.createdAt.toDate(),
+          },
+        })) as Chat[]
+      ).sort((a, b) =>
+        (a.recentMessage?.createdAt ?? a.updatedAt) > (b.recentMessage?.createdAt ?? b.updatedAt)
+          ? -1
+          : 1
+      ) // 最新の更新順で並び替え
     : []
 
   return { chats, isLoading, error }
