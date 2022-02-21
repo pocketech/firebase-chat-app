79485b1ed449cbb061a1bdef2183fb12591a72e8
pocketech <hakuyowasedatech@gmail.com>
2022-02-21 19:40:16 +0900

    :label:

---
+++ functions/src/types/providerId.ts
@@ -0,0 +1,8 @@
+// @see https://blog.katsubemakito.net/firebase/firebase-authentication-multi-web1
+export type ProviderId =
+  | 'password'
+  | 'phone'
+  | 'google.com'
+  | 'facebook.com'
+  | 'twitter.com'
+  | 'github.com'
