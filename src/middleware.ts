134b8367072a546383e6b7183d0ac00dffa3b715
pocketech <hakuyowasedatech@gmail.com>
2022-07-25 18:02:45 +0900

    :wrench:ミドルウェアの書き換え

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,24 +3,20 @@ import { NextResponse } from 'next/server'
 
 // Basic認証
 export const middleware = (req: NextRequest) => {
-  if (!process.env.USE_BASIC_AUTH) return
+  if (process.env.USE_BASIC_AUTH !== 'enabled') return
 
   const basicAuth = req.headers.get('authorization')
 
   if (basicAuth) {
     const auth = basicAuth.split(' ')[1]
-    const [user, password] = Buffer.from(auth, 'base64').toString().split(':')
+    const [user, password] = atob(auth).split(':')
 
     if (user === process.env.BASIC_AUTH_USER && password === process.env.BASIC_AUTH_PASSWORD) {
       return NextResponse.next()
     }
   }
 
-  return new Response('Auth required', {
-    status: 401,
-    headers: {
-      // eslint-disable-next-line @typescript-eslint/naming-convention
-      'WWW-Authenticate': 'Basic realm="Secure Area"',
-    },
-  })
+  const authUrl = new URL('/api/auth', req.url)
+
+  return NextResponse.rewrite(authUrl)
 }
